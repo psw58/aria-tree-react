858e32c365582a7928c25dc0d56eeee3c0c81227
Philip Williammee <psw58@cornell.edu>
2018-08-17 21:30:57 -0400

    updates

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import HelloWorld from './js/hello_world';
 import {MyButton} from './js/events';
 
 import { MyComponent } from './js/service';
@@ -9,20 +8,12 @@ import { MyComponent } from './js/service';
 //styles
 import './css/_cwd_events.scss';
 
-ReactDOM.render(
-      <HelloWorld name="test" />,
-    document.getElementById('hello_world')
-);
 
 ReactDOM.render(
   <MyButton />,
-    document.getElementById('events')  
+    document.getElementById('root')  
 );
 
-ReactDOM.render(
-  <MyComponent subreddit="reactjs" />,
-  document.getElementById('dynamic')  
-);
 
 
 
