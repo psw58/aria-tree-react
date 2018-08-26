314c6b1d128454d3a33bed8fd849f6779bcad44c
philwilliammee <philliammee@yahoo.com>
2018-08-25 22:33:19 -0400

    clean up and arrowdown

--- src/js/events.jsx
+++ src/js/events.jsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react';
 //import KeyboardEventHandler from 'react-keyboard-event-handler';
 
 var nodes = [
-	{id:0, name:'root', type:'root', parent:'', isParent:true, expanded:false, focus:false, groups:[1,2,3,7,8], tabIndex:0 },
-	{id:1, name:'', type:'child', parent:0,  isParent:false, focus:false, tabIndex:-1 },
-	{id:2, name:'', type:'child', parent:0,  isParent:false, focus:false, tabIndex:-1 },
-	{id:3, name:'', type:'', parent:'', isParent:true, expanded:false, focus:false, groups:[4,5,6], tabIndex:-1 },
-	{id:4, name:'', type:'child', parent:3,  isParent:false, focus:false, tabIndex:-1 },
-	{id:5, name:'', type:'child', parent:3,  isParent:false, focus:false, tabIndex:-1 },
-	{id:6, name:'', type:'child', parent:3,  isParent:false, focus:false, tabIndex:-1 },
-	{id:7, name:'', type:'child', parent:3,  isParent:false, focus:false, tabIndex:-1 },
-	{id:8, name:'', type:'', parent:'', isParent:true, expanded:false, focus:false, groups:[9,10], tabIndex:-1 },
-	{id:9, name:'', type:'child', parent:9,  isParent:false, focus:false, tabIndex:-1 },
-	{id:10, name:'', type:'child', parent:10,  isParent:false, focus:false, tabIndex:-1 },	
+	{id:0, name:'root', type:'root', parent:'', visable: true, isParent:true, expanded:false, focus:false, groups:[1,2,3,7,8], tabIndex:0 },
+	{id:1, name:'', type:'child', parent:0, visable: false, isParent:false, focus:false, tabIndex:-1 },
+	{id:2, name:'', type:'child', parent:0, visable: false, isParent:false, focus:false, tabIndex:-1 },
+	{id:3, name:'', type:'', parent:0, visable: false, isParent:true, expanded:false, focus:false, groups:[4,5,6], tabIndex:-1 },
+	{id:4, name:'', type:'child', parent:3,  visable: false, isParent:false, focus:false, tabIndex:-1 },
+	{id:5, name:'', type:'child', parent:3,  visable: false, isParent:false, focus:false, tabIndex:-1 },
+	{id:6, name:'', type:'child', parent:3, visable: false,  isParent:false, focus:false, tabIndex:-1 },
+	{id:7, name:'', type:'child', parent:0, visable: false, isParent:false, focus:false, tabIndex:-1 },
+	{id:8, name:'', type:'', parent:0, visable: false, isParent:true, expanded:false, focus:false, groups:[9,10], tabIndex:-1 },
+	{id:9, name:'', type:'child', parent:9, visable: false, isParent:false, focus:false, tabIndex:-1 },
+	{id:10, name:'', type:'child', parent:10, visable: false, isParent:false, focus:false, tabIndex:-1 },	
 ];
 
 /*
@@ -33,35 +33,69 @@ export class TreeView extends Component {
 		  nodes: nodes,
 		  tid:0
 		};
-		console.log(this.state.nodes[this.state.tid].expanded);
-		console.log(this.state.nodes[this.state.tid].type);
 	  }  
 
 	onClickEvent( event ){
 		//@TODO if this element does not have focus give it focus
-		event.preventDefault();
-		console.log(event.target);
-		
-		//console.log(this.state.nodes[this.state.tid].expanded)
+		const oldID = this.state.tid;
+		if ( event.target.dataset.id ){
+			this.state.tid = parseInt(event.target.dataset.id);
+		}else{
+			console.log('error no data id')
+		}
+		//clicked a childs span element not a node
 		if (event.target.id == "span"+this.state.tid){
 			this.state.nodes[this.state.tid].expanded = !this.state.nodes[this.state.tid].expanded;
 		}
-		
+		this.setNodeVisibleState(this.state.tid)
 		this.setState({nodes: this.state.nodes});
-
+		this.moveFocus(oldID, this.state.tid)
 	}
 
-	onFocusEvent (e){
+	setNodeVisibleState(stateID){
+		const myID = stateID;
+		if ('groups' in this.state.nodes[myID]){
+			this.state.nodes[myID].groups.forEach(element => {
+				//if it has a child node set visible to false
+				if (this.state.nodes[element].groups){
+					console.log("setting id " + this.state.nodes[element].id + " children to unsearchable");
+					this.state.nodes[element].groups.forEach(el => {
+						this.state.nodes[el].visable = false;
+					})
+				}				
+				if(this.state.nodes[myID].expanded){
+					//visable is true
+					this.state.nodes[element].visable = true;
+					console.log('setting child visible to true');
+				}else{
+					//visivle is false
+					this.state.nodes[element].visable = false;
+				}
+			});
+		}else{
+			console.log('no groups');
+		}
+	}
+
+	moveFocus(curID, nextID){
+		//remove focus from old element 
+		this.state.nodes[curID].focus = false;
+		this.state.nodes[curID].tabIndex = -1;
+		//set focus to current elem
+		this.state.nodes[nextID].focus = true;
+		this.state.nodes[nextID].tabIndex = 0;
+		this.setState({nodes: this.state.nodes});
+		this.refs[nextID].focus();
+	}
+
+	onFocusEvent(e){
 		this.state.nodes[this.state.tid].focus = true;
+		this.state.nodes[this.state.tid].tabIndex = 0;
 		this.setState({nodes: this.state.nodes});
 	}
 
 	onBlurEvent(e){
 		this.state.nodes[this.state.tid].focus = false;
-
-		//return to this node @todo this needs testing
-		this.state.nodes[this.state.tid].tabIndex = 0;
-
 		this.setState({nodes: this.state.nodes});
 	}
 
@@ -75,9 +109,10 @@ export class TreeView extends Component {
 			*/
 			case 'Enter':
 				if ( 'expanded' in this.state.nodes[this.state.tid] ){
-					this.state.nodes[this.state.tid].expanded = !this.state.nodes[this.state.tid].expanded 
+					this.state.nodes[this.state.tid].expanded = !this.state.nodes[this.state.tid].expanded;
+					this.setNodeVisibleState(this.state.tid)
 				}else{
-					//@todo set focus to this element
+					//element can not expand ignore enter key
 					console.log('element can not expand');
 				}
 				break;
@@ -85,110 +120,65 @@ export class TreeView extends Component {
 				Moves focus to the next node that is focusable without opening or closing a node.
 			*/
 			case 'ArrowDown':
-				//@todo this should only move to child element if it is visable
-				//remove focus from last element
-				this.state.nodes[this.state.tid].tabIndex = -1;
-				this.refs[this.state.tid].blur()
-				//@TOD UPDATE THE TARGET ONLY IF ITS VISABLE
-				//update current target id
-
-				//if the element is a expanded list move to next child elemnt in sub list
-				if (this.state.nodes[this.state.tid].expanded == true){
-					console.log('expanded folder moving to next element');
-					if (nodes.length > this.state.tid+1){
-						this.state.tid += 1;
-					}else{
-						//loop back to begining node
-						console.log("end of nodes looping to top");
-						this.state.tid = 0
-					}
-				//else if the element is a child of a expanded folder
-				}else if( !('expanded'  in this.state.nodes[this.state.tid]) ){
-					console.log('file found moving to next element')
-					//else its not expanded find next visible node
-					if (nodes.length > this.state.tid+1){
-						this.state.tid += 1;
-					}else{
-						//loop back to begining node
-						console.log("end of nodes looping to top");
-						this.state.tid = 0
-					}
-				//the element is a collapsed folder move to the next visable element
-				}else if(this.state.nodes[this.state.tid].expanded == false){
-					console.log('collapsed folder find next file')
-					var val = this.state.nodes[this.state.tid]			
-					function sortNumber(a,b) {
-						return a - b;
-					}
-				
-					if ('groups' in val){
-						//sort the children in case they arent sorted
-						var groups = val.groups.sort(sortNumber);
-						//return the last element in the array
-						var myTID = groups[groups.length-1];
-						console.log(myTID);
-						if (nodes.length > myTID+1){
-							console.log(this.state.nodes[myTID]);
-							//the last node is a group but it is not visable find its last child node
-							if ('groups'  in this.state.nodes[myTID]){
-								var val2 = this.state.nodes[myTID];
-								var groups2 = val2.groups.sort(sortNumber);
-								myTID = groups2[groups2.length-1];
-
-								if (nodes.length > myTID+1){
-									console.log('setting tid to '+(myTID + 1))
-									this.state.tid =  myTID + 1;
-								}else{
-									//loop back to begining node
-									console.log("end of nodes looping to top");
-									this.state.tid = 0
-								}								
-							}else{
-								//it is a list element file move to it
-								console.log('found available file list elemet')
-								this.state.tid = myTID + 1;;
-							}	
-						}else{
-							//loop back to begining node
-							console.log("end of nodes looping to top");
+				//save id to remove focus from last element
+				const oldID = this.state.tid;
+				if (this.state.nodes.length > this.state.tid+1){
+					//find closest visable item
+					for (var i=this.state.tid+1; i<this.state.nodes.length; i++){
+						var elem = this.state.nodes[i];
+						if ( ('visable' in elem) && (elem.visable == true)){
+							this.state.tid = i;
+							break;
+						}else if ( i == this.state.nodes.length-1){
+							//no loops are found
+							console.log('no visable element found return to root');
 							this.state.tid = 0
+						}else{
+							console.log('error no visable elem');
 						}	
-					}else{
-						console.log('ERROR in :');
-						console.log(val);
-					}					
-					console.log(val);
-			
-
+					}	
 				}else{
-					console.log('ERROR no element found')
+					//your on last node loop back to begining node
+					console.log("beggining of nodes looping to root");
+					this.state.tid = 0
 				}
-				//set focus to this element
-				this.refs[this.state.tid].focus()
-				//set its tabindex to 0
-				this.state.nodes[this.state.tid].tabIndex = 0;
+				
+				//set focus to this element		
+				this.moveFocus(oldID, this.state.tid);
+
 				break;
 			/* Up Arrow: 
 				Moves focus to the previous node that is focusable without opening or closing a node.
 			*/
 			case 'ArrowUp':
-				//@todo this should only move to child element if it is visable
-				//remove focus from last element
-				this.state.nodes[this.state.tid].tabIndex = -1;
-				this.refs[this.state.tid].blur();
+				//save id to remove focus from last element
+				const oldID2 = this.state.tid;
 				//update current target id
 				//@TOD0 UPDATE THE TARGET ONLY IF ITS VISABLE
 				if (this.state.tid > 0){
-					this.state.tid -= 1;
+					//find closest visable item
+					for (var i = this.state.tid-1; i >= 0; --i){
+						if (this.state.nodes[i].visable == true){
+							this.state.tid = i;
+							break; 
+						}
+					}	
 				}else{
 					//loop back to begining node
 					console.log("beggining of nodes looping to botttom");
 					this.state.tid = nodes.length-1
+					//find closest visable item
+					for (var i = this.state.tid; i >= 0; --i){
+						if (this.state.nodes[i].visable == true){
+							this.state.tid = i;
+							break; 
+						}
+					}					
 				}
-				//set focus to this element
-				this.refs[this.state.tid].focus()
-				this.state.nodes[this.state.tid].tabIndex = 0;
+
+				this.moveFocus(oldID2, this.state.tid);
 				break;
+
 			/* Right arrow:
 				When focus is on a closed node, opens the node; focus does not move.
 				When focus is on a open node, moves focus to the first child node.
@@ -248,10 +238,13 @@ export class TreeView extends Component {
 				  	aria-expanded={this.state.nodes[0].expanded} 
 					tabIndex={this.state.nodes[0].tabIndex}  
 					ref={this.state.nodes[0].id}
+					data-visable = {this.state.nodes[0].visable} 
+					data-id={this.state.nodes[0].id}
 				>
 					<span 
 						className={this.state.nodes[0].focus ? 'focus' : ''} 
 						id={'span'+this.state.nodes[0].id} 
+						data-id={this.state.nodes[0].id}
 					>
 						Projects
 					</span>
@@ -261,6 +254,8 @@ export class TreeView extends Component {
 					  tabIndex={this.state.nodes[1].tabIndex}
 					  className={this.state.nodes[1].focus ? 'focus doc' : 'doc' } 
 					  ref={this.state.nodes[1].id}
+					  data-visable = {this.state.nodes[1].visable} 
+					  data-id={this.state.nodes[1].id}
 				>
 					project-1.docx
 				  </li>
@@ -268,6 +263,8 @@ export class TreeView extends Component {
 					  tabIndex={this.state.nodes[2].tabIndex}
 					  className={this.state.nodes[2].focus ? 'focus doc' : 'doc' } 
 					  ref={this.state.nodes[2].id}
+					  data-visable = {this.state.nodes[2].visable} 
+					  data-id={this.state.nodes[2].id}
 				>
 					project-2.docx
 				  </li>
@@ -275,10 +272,13 @@ export class TreeView extends Component {
 						aria-expanded={this.state.nodes[3].expanded} 
 						tabIndex={this.state.nodes[3].tabIndex}  
 						ref={this.state.nodes[3].id}
+						data-visable = {this.state.nodes[3].visable} 
+						data-id={this.state.nodes[3].id}
 				  >
 					<span
 						className={this.state.nodes[3].focus ? 'focus' : ''} 
 						id={'span'+this.state.nodes[3].id} 
+						data-id={this.state.nodes[3].id}
 					>
 					  Project 3
 					</span>
@@ -286,14 +286,18 @@ export class TreeView extends Component {
 					  <li role="treeitem" 
 						tabIndex={this.state.nodes[4].tabIndex}
 						className={this.state.nodes[4].focus ? 'focus doc' : 'doc' } 
-						ref={this.state.nodes[4].id}			  
+						ref={this.state.nodes[4].id}
+						data-visable = {this.state.nodes[4].visable} 	
+						data-id={this.state.nodes[4].id}		  
 					  >
 						project-3A.docx
 					  </li>
 					  <li role="treeitem" 
 						tabIndex={this.state.nodes[5].tabIndex}
 						className={this.state.nodes[5].focus ? 'focus doc' : 'doc' } 
-						ref={this.state.nodes[5].id}	
+						ref={this.state.nodes[5].id}
+						data-visable = {this.state.nodes[5].visable} 
+						data-id={this.state.nodes[5].id}	
 						>
 						project-3B.docx
 					  </li>
@@ -301,6 +305,8 @@ export class TreeView extends Component {
 						tabIndex={this.state.nodes[6].tabIndex}
 						className={this.state.nodes[6].focus ? 'focus doc' : 'doc' } 
 						ref={this.state.nodes[6].id}	
+						data-visable = {this.state.nodes[6].visable} 
+						data-id={this.state.nodes[6].id}
 					  >
 						project-3C.docx
 					  </li>
@@ -310,6 +316,8 @@ export class TreeView extends Component {
 						tabIndex={this.state.nodes[7].tabIndex}
 						className={this.state.nodes[7].focus ? 'focus doc' : 'doc' } 
 						ref={this.state.nodes[7].id}	
+						data-visable = {this.state.nodes[7].visable} 
+						data-id={this.state.nodes[7].id}
 					  >
 					project-4.docx
 				  </li>
@@ -317,10 +325,13 @@ export class TreeView extends Component {
 						aria-expanded={this.state.nodes[8].expanded} 
 						tabIndex={this.state.nodes[8].tabIndex}  
 						ref={this.state.nodes[8].id}
+						data-visable = {this.state.nodes[8].visable} 
+						data-id={this.state.nodes[8].id}
 				  >
 					<span
 						className={this.state.nodes[8].focus ? 'focus' : ''} 
 						id={'span'+this.state.nodes[8].id} 
+						data-id={this.state.nodes[8].id}
 					>
 					  Project 5
 					</span>
@@ -328,7 +339,9 @@ export class TreeView extends Component {
 					<li role="treeitem" 
 						tabIndex={this.state.nodes[9].tabIndex}
 						className={this.state.nodes[9].focus ? 'focus doc' : 'doc' } 
-						ref={this.state.nodes[9].id}	
+						ref={this.state.nodes[9].id}
+						data-visable = {this.state.nodes[9].visable} 	
+						data-id={this.state.nodes[9].id}
 					  >
 						project-5A.docx
 					</li>
@@ -336,6 +349,8 @@ export class TreeView extends Component {
 						tabIndex={this.state.nodes[10].tabIndex}
 						className={this.state.nodes[10].focus ? 'focus doc' : 'doc' } 
 						ref={this.state.nodes[10].id}	
+						data-visable = {this.state.nodes[10].visable} 
+						data-id={this.state.nodes[10].id}
 					  >
 						project-5B.docx
 					  </li>
@@ -351,3 +366,79 @@ export class TreeView extends Component {
 }
 
 
+
+/* old arrow down
+//if the element is a expanded list move to next child elemnt in sub list
+if (this.state.nodes[this.state.tid].expanded == true){
+	console.log('expanded folder moving to next element');
+	if (nodes.length > this.state.tid+1){
+		this.state.tid += 1;
+	}else{
+		//loop back to begining node
+		console.log("end of nodes looping to top");
+		this.state.tid = 0
+	}
+//else if the element is a child of a expanded folder
+}else if( !('expanded'  in this.state.nodes[this.state.tid]) ){
+	console.log('file found moving to next element')
+	//else its not expanded find next visible node
+	if (nodes.length > this.state.tid+1){
+		this.state.tid += 1;
+	}else{
+		//loop back to begining node
+		console.log("end of nodes looping to top");
+		this.state.tid = 0
+	}
+//the element is a collapsed folder move to the next visable element
+}else if(this.state.nodes[this.state.tid].expanded == false){
+	console.log('collapsed folder find next file')
+	var val = this.state.nodes[this.state.tid]			
+	function sortNumber(a,b) {
+		return a - b;
+	}
+
+	if ('groups' in val){
+		//sort the children in case they arent sorted
+		var groups = val.groups.sort(sortNumber);
+		//return the last element in the array
+		var myTID = groups[groups.length-1];
+		console.log( myTID )
+		console.log(myTID);
+		if (nodes.length > myTID+1){
+			console.log(this.state.nodes[myTID]);
+			//the last node is a group but it is not visable find its last child node
+			if ('groups'  in this.state.nodes[myTID]){
+				var val2 = this.state.nodes[myTID];
+				var groups2 = val2.groups.sort(sortNumber);
+				myTID = groups2[groups2.length-1];
+
+				if (nodes.length > myTID+1){
+					console.log('setting tid to '+(myTID + 1))
+					this.state.tid =  myTID + 1;
+				}else{
+					//loop back to begining node
+					console.log("end of nodes looping to top");
+					this.state.tid = 0
+				}								
+			}else{
+				//it is a list element file move to it
+				console.log('found available file list elemet')
+				this.state.tid = myTID + 1;;
+			}	
+		}else{
+			//loop back to begining node
+			console.log("end of nodes looping to top");
+			this.state.tid = 0
+		}	
+	}else{
+		console.log('ERROR in :');
+		console.log(val);
+	}					
+	console.log(val);
+	
+
+
+}else{
+	console.log('ERROR no element found')
+}
+*/
